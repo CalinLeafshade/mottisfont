2785277e33d65a64b83dc9aa4d9524636e376086
Steven Poulton <poulton.steven@googlemail.com>
2015-05-26 10:25:53 +0100

    fixed topic links and added name and topic to topic view

--- site/js/site.js
+++ site/js/site.js
@@ -2,43 +2,51 @@
 var currentPerson;
 
 function backToMain () {
-	var t = $('#main-template').html();
-	$('.container').html(Mustache.render(t, Model));	
-	currentPerson = null;
+    var t = $('#main-template').html();
+    $('.container').html(Mustache.render(t, Model));
+    currentPerson = null;
 }
 
 function gotoPerson (name) {
-	var t = $('#person-template').html();
-	currentPerson = Model.personByName(name);
-	$('.container').html(Mustache.render(t, currentPerson));
+    var t = $('#person-template').html();
+    currentPerson = Model.personByName(name);
+    var view = {
+        name: currentPerson.name,
+        topics: currentPerson.topics.map(function(e,i) {
+          return { id: i, text: e };
+        })
+    }
+    $('.container').html(Mustache.render(t, view));
 }
 
 function gotoTopic (idx) {
-	var view = {
-		video: currentPerson.topicVideo(idx)
-	}
-	var t = $('#topic-template').html();
-	$('.container').html(Mustache.render(t, view));
+    var view = {
+        video: currentPerson.topicVideo(idx),
+        name: currentPerson.name,
+        topic: currentPerson.topics[idx]
+    }
+    var t = $('#topic-template').html();
+    $('.container').html(Mustache.render(t, view));
 }
 
 $(function() {
-	backToMain();	
+    backToMain();
 
-	$('body').on('click', '.nav-link', function (ev) {
-		var target = $(this).data('target');
-		if (target == "topic") {
-			var index = $(this).index();
-			gotoTopic(index);	
-		}
-		else if (target == "main") {
-			backToMain();
-		}
-		else if (target == "person") {
-			gotoPerson(currentPerson.name);
-		}
-		else {
-			gotoPerson($(this).data('name'));
-		}
-	});
+    $('body').on('click', '.nav-link', function (ev) {
+        var target = $(this).data('target');
+        if (target == "topic") {
+            var index = Number($(this).data('index'));
+            gotoTopic(index);
+        }
+        else if (target == "main") {
+            backToMain();
+        }
+        else if (target == "person") {
+            gotoPerson(currentPerson.name);
+        }
+        else {
+            gotoPerson($(this).data('name'));
+        }
+    });
 
 });
