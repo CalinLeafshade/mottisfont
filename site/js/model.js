9b6934fe3ae68a4cedfd7930ccc225a65a242587
uoou <uo.is.ou@gmail.com>
2015-05-27 15:08:28 +0100

    scones

--- site/js/model.js
+++ site/js/model.js
@@ -32,7 +32,7 @@ Model = {
         new Person("Catherine", [ "Catherine, the Outdoor Ranger", "Catherine and The Nuthatch", "Catherine's Flock", "Foraging", "Why I love the seasons", "<span class=\"biggo\">Zoonotic Diseases</span><span class=\"smallo\">(may put you off your scone!)</span>" ]),
         new Person("Dylan", [ "Dylan, the Countryside Manager", "Change at Mottisfont", "Conservation & Visitor access", "Mottisfont history", "Mottisfont is very special to me", "Challenging and enjoyable roles" ]),
         new Person("Helen", [ "My Journey to Mottisfont","I didn't expect the challenges", "I was the wild card", "Mottisfont has so much to offer", "Mottisfont means everything to me", "One of many moments at Mottisfont" ]),
-        new Person("Howard", [ "I'm Howard, a Volunteer", "About Mottisfont", "Indian encounter", "Mottisfont is a thin place", "Reaching Beyond Religion", "The Font" ]),
+        new Person("Howard", [ "I'm Howard, a Volunteer", "About Mottisfont", "The meaning of water at Mottisfont", "Mottisfont is a thin place", "Reaching Beyond Religion", "The Font" ]),
         new Person("Jonny", [ "Jonny, the Head Gardener", "Challenges in my role", "How I became a gardener","My passion for gardening", "Roses research", "the Mottisfont Roses" ]),
         new Person("Louise", [ "My Journey to Mottisfont", "Hospitality at Mottisfont", "Magical Mottisfont", "Mottisfont, Art and Maud Russell", "The National Trust", "The Whistler Room" ])
     ],
