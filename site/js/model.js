88e6363d184a49072f4acf82726dd2b10bb8da61
uoou <uo.is.ou@gmail.com>
2015-05-27 14:23:14 +0100

    lol

--- site/js/model.js
+++ site/js/model.js
@@ -26,12 +26,12 @@ Person.prototype.topicVideo = function(idx) {
 
 Model = {
     people: [
-        new Person("Catherine", [ "Catherine and The Nuthatch", "Catherine's Flock", "Foraging", "Why I love the Seasons at Mottisfont", "Zoonotic Diseases" ]),
-        new Person("Dylan", [ "Change at Mottisfont", "Conservation and Visitor access", "Mottisfont History", "Mottisfont is very special to me", "Challenging and enjoyable roles" ]),
-        new Person("Helen", [ "I didn't expect the Challenges", "I was the wild card", "Mottisfont has so much to offer", "Mottisfont means everything to me", "One of many moments at Mottisfont" ]),
-        new Person("Howard", [ "About Mottisfont", "Indian encounter", "Mottisfont is a thin place", "Reaching Beyond Religion", "The Font" ]),
-        new Person("Jonny", [ "Challenges in my role", "My passion for gardening", "Roses research", "the Mottisfont Roses" ]),
-        new Person("Louise", [ "Hospitality at Mottisfont", "Magical Mottisfont", "Mottisfont, Art and Maud Russell", "The National Trust", "The Whistler Room" ])
+        new Person("Catherine", [ i"Catherine, the Outdoor Ranger", "Catherine and The Nuthatch", "Catherine's Flock", "Foraging", "Why I love the seasons", "Zoonotic Diseases (may putt you off your scone!)" ]),
+        new Person("Dylan", [ "Dylan, the Countryside Manager", "Change at Mottisfont", "Conservation & Visitor access", "Mottisfont history", "Mottisfont is very special to me", "Challenging and enjoyable roles" ]),
+        new Person("Helen", [ "My Journey to Mottisfont","I didn't expect the challenges", "I was the wild card", "Mottisfont has so much to offer", "Mottisfont means everything to me", "One of many moments at Mottisfont" ]),
+        new Person("Howard", [ i"I'm Howard, a Volunteer", "About Mottisfont", "Indian encounter", "Mottisfont is a thin place", "Reaching Beyond Religion", "The Font" ]),
+        new Person("Jonny", [ "Jonny, the Head Gardener", "Challenges in my role", "How I became a gardener","My passion for gardening", "Roses research", "the Mottisfont Roses" ]),
+        new Person("Louise", [ "My Journey to Mottisfont", "Hospitality at Mottisfont", "Magical Mottisfont", "Mottisfont, Art and Maud Russell", "The National Trust", "The Whistler Room" ])
     ],
     personByName: function (name) {
         return Model.people.find(function (e) {
