073f2a6779b3878d8d931c06c002c9d555a2fbb3
uoou <uo.is.ou@gmail.com>
2015-05-28 03:19:17 +0100

    launcher

---
+++ runchrome/main.lua
@@ -0,0 +1,19 @@
+function love.load()
+	love.window.setMode(1080, 1920, {fullscreen=true, fullscreentype="desktop"})
+	image = love.graphics.newImage("image.jpg")
+end
+function love.draw()
+	love.graphics.draw(image, 0, 0)
+end
+function love.mousereleased(x, y, button)
+	if button == "l" then
+		if os.execute("google-chrome --kiosk http://localhost:3000/ &") then
+			love.event.push ( "quit" )
+		end
+	end
+end
+function love.focus (f)
+	if not f then
+		love.event.push ( "quit" )
+	end
+end
